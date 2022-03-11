ebda561f9362d89d7c803e043a1448ce4d511875
Samie <samrasolomon40@gmail.com>
2022-03-11 18:23:29 +0000

    Added a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 file { '/tmp/school':
-  path => "/tmp/school",
-  ensure  => "present",
-  owner   => "www-data",
-  group   => "www-data",
-  mode    => "0744",
-  content  => "I love Puppet",
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content  => 'I love Puppet',
 }
