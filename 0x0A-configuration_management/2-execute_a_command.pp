389f27d142a240e5ce00b4f044d3d35517329f89
Samie <samrasolomon40@gmail.com>
2022-03-11 19:49:48 +0000

    Added a file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # This will kill a process using pkill
-class exec {
-  exec { 'kill a process':
-    command => 'pkill -9 -f killmenow',
-  }
+exec { 'kill a process':
+  command => 'pkill -9 -f killmenow',
 }
+
