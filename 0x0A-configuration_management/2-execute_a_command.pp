7671febc67bed79953543a338f9b215a900c11b5
Samie <samrasolomon40@gmail.com>
2022-03-11 20:03:38 +0000

    Added a file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
 # This will kill a process using pkill
 Exec { 'kill a process':
-  command => 'pkill -9 -f /home/vagrant/alx-system_engineering-devops/0x0A-configuration_management/killmenow',
+  command => '/usr/bin/pkill /home/vagrant/alx-system_engineering-devops/0x0A-configuration_management/killmenow',
 }
