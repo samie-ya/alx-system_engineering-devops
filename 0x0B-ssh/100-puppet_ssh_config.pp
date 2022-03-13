4e4b75d4d5907770f06d047d07416833b04e34b0
Samie <samrasolomon40@gmail.com>
2022-03-13 19:14:13 +0000

    Modified a file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # This script will use Puppet to make changes to our configuration file
-class { 'ssh::client':
-  storeconfigs_enabled => false,
-  options              => {
-    'Host *' => {
-      'HostName'                => '34.148.148.119',
-      'PasswordAuthenitication' => 'No',
-      'IdentityFile'            => '~/.ssh/school',
-    },
-  },
+node default {
+  include ssh::client
+  class { 'ssh::client':
+    option => {
+      'Host *' => {
+        'HostName'                => '34.148.148.119',
+        'PasswordAuthenitication' => 'No',
+        'IdentityFile'            => '~/.ssh/school',
+      }
+    }
+  }
 }
