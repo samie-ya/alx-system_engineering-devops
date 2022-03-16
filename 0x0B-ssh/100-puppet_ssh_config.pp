02cac7e5ca4aa86967dcacf51e7da8143916691d
Samie <samrasolomon40@gmail.com>
2022-03-16 18:07:09 +0000

    Modified a file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,12 @@
 # This script will use Puppet to make changes to our configuration fi
-node default {
-  include stdlib
-  file_line { 'ssh config':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => 'PasswordAuthenitication No', 'IdentityFile ~/.ssh/school',
-  }
+include stdlib
+file_line { 'ssh config':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
+file_line { 'ssh config':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthenitication No',
 }
