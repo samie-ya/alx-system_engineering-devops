d1ee179f7ad21ecb6ce3e7e6a3bbcad07505157d
Samie <samrasolomon40@gmail.com>
2022-03-16 01:09:15 +0000

    Added a file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,6 +5,14 @@ file_line { 'ssh config':
   ensure => present,
   path   => '/etc/ssh/config',
   line   => 'HostName 34.148.148.119',
-  line1  => 'PasswordAuthenitication No',
+}
+file_line { 'ssh config':
+  ensure => present,
+  path   => '/etc/ssh/config',
+  line   => 'PasswordAuthenitication No',
+}
+file_line { 'ssh config':
+  ensure => present,
+  path   => '/etc/ssh/config',
   line2  => 'IdentityFile ~/.ssh/school',
 }
