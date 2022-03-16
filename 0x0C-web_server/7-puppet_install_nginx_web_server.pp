e4554ad87e0683b99387e816569557f4160ad7b3
Samie <samrasolomon40@gmail.com>
2022-03-16 19:29:37 +0000

    Modified a file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,9 +1,32 @@
 # This script will install nginx and configure it
-exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+include stdlib
+
+exec { 'sudo apt-get update':
+  command => '/usr/bin/apt-get update'
 }
 
 package { 'nginx':
-    ensure  => 'installed',
-    require => Exec['apt-get update']
+  ensure  => 'installed',
+  require => Exec['apt-get update']
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => "present",
+  content => "Hello World!",
+  notify  => Service['nginx'],
+  require => Package['nginx']
+}
+
+file_line { '301 permanent redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'server_name 34.148.148.119;\n\n\tlocation /redirect_me {\n\t\treturn 301 "https://www.youtube.com/watch?v=QH2-TGUlwu4" ;\n\t}\n',
+  match  => '^server_name _;',
+  notify  => Service['nginx'],
+  require => Package['nginx']
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true
 }
