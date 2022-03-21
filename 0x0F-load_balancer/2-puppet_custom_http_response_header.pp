6bd6e37a12f27ffeac0b3bdee1e82c611496178a
Samie <samrasolomon40@gmail.com>
2022-03-21 19:14:58 +0000

    Modified a file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,4 @@
 # This script will install nginx and configure it and its header
 exec { 'the bash script':
-  command => './0-custom_http_response_header'
+  command => './home/vagrant/alx-system_engineering-devops/0x0F-load_balancer/0-custom_http_response_header'
 }
