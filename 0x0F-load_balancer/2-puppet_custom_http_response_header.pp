5d1929e4465d355f81709baf1c36e905905a3c95
Samie <samrasolomon40@gmail.com>
2022-03-21 19:47:22 +0000

    Modified a file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,5 @@
 # This script will install nginx and configure it and its header
-exec { './0-custom_http_response_header':
-  command => '/home/vagrant/alx-system_engineering-devops/0x0F-load_balancer/0-custom_http_response_header'
+exec { 'creating custom headers':
+  command  => 'name=$(uname -n); sudo apt-get update; sudo apt-get -y install nginx; sudo ufw --force enable; sudo ufw allow "Nginx HTTP"; sudo sed -i -e "s+location / {+location / {\n\t\tadd_header X-Served-By \"$name\";+" /etc/nginx/sites-available/default; sudo service nginx restart;',
+  provider => shell,
 }
