a53f4be1d915a65616a9663e9fa4158060777a50
Samie <samrasolomon40@gmail.com>
2022-03-21 19:09:31 +0000

    Added files

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,4 @@
-# This script will install nginx and configure it
-class { 'nginx':
-  exec { 'apt-get update':
-   command => '/usr/bin/apt-get update'
-  }
-
-  package { 'nginx':
-    ensure  => 'installed',
-    require => Exec['apt-get update'],
-  }
-
-  service { 'nginx':
-    ensure  => running,
-    require => Package['nginx'],
-  }
-
-  nginx::resource::vhost { 'status':
-    listen_port => '80',
-    headers     => 'set X-Served-By "1359-web-02"',
-  }
+# This script will install nginx and configure it and its header
+exec { 'the bash script':
+  command => './0-custom_http_response_header'
 }
