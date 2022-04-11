afe9c55e3c34ad857c2b7ed1741afcf678fd96d0
samie-ya <samrasolomon40@gmail.com>
2022-04-11 20:59:30 +0000

    Modified a file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -25,6 +25,6 @@ if __name__ == "__main__":
         i['username'] = user_name
     file_name = sys.argv[1] + ".csv"
     with open(file_name, "w", encoding='utf-8') as new_file:
-        csv_file = csv.writer(new_file, quoting=csv.QUOTE_ALL)
+        csv_file = csv.writer(new_file, quoting=csv.QUOTE_ALL, delimiter=',')
         for i in id_list:
             csv_file.writerow(i.values())
