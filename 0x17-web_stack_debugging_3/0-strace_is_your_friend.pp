69c3e94a574316fef8578529989e79fce154a1f4
samie-ya <samrasolomon40@gmail.com>
2022-05-02 00:31:23 +0000

    Modified error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # This script will debug apache
 exec { 'debugging apache':
-  command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-setting.php;',
+  command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
   provider => shell,
 }
