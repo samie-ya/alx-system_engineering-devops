5ca5486e0afe1f2ca57fc01832ec0e0ee6aa00a2
samie-ya <samrasolomon40@gmail.com>
2022-05-03 17:09:55 +0000

    Added files

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# This script will fix too many files error
+exec { 'configure nginx':
+  command  => 'sed -i -e "s/-n 15/-n 4096/" /etc/default/nginx; service nginx restart',
+  provider => shell,
+}
